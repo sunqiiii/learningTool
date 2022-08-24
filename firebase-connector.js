f2391c8ea4378d6faa392a0caa02c3cf22ed6d34
sunqiiii <108568986+sunqiiii@users.noreply.github.com>
2022-08-24 11:28:49 +0800

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,3 +1,7 @@
+import { getDatabase } from "firebase/database";
+
+const database = getDatabase();
+
 firebase.database().ref();  // Specifies the database root
 firebase.database().ref("child/path") // Specifies a specific location in the database tree
 
