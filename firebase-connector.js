bc3c4f0bc85fcab673c4773d867e1df16498cb00
sunqiiii <108568986+sunqiiii@users.noreply.github.com>
2022-08-25 09:32:32 +0800

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -4,14 +4,14 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-a
 import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
 function initializeFirebase() {
   var firebaseConfig = {
-    apiKey: "AIzaSyB7PyoBopqhHlDIeMtIfZc9tP2PyWdPEOQ",
-    authDomain: "best-learning-tool-9281c.firebaseapp.com",
-    databaseURL: "https://best-learning-tool-9281c-default-rtdb.firebaseio.com",
-    projectId: "best-learning-tool-9281c",
-    storageBucket: "best-learning-tool-9281c.appspot.com",
-    messagingSenderId: "988183622580",
-    appId: "1:988183622580:web:9677979a51d67f58d0f8e1",
-    measurementId: "G-B6YY6VSY25"
+    apiKey: "AIzaSyDW93BmUlsdrp3MBc_rBRLPKeZViv9OAk4",
+    authDomain: "best-learning-tool-42ca7.firebaseapp.com",
+    databaseURL: "https://best-learning-tool-42ca7-default-rtdb.firebaseio.com",
+    projectId: "best-learning-tool-42ca7",
+    storageBucket: "best-learning-tool-42ca7.appspot.com",
+    messagingSenderId: "230725901175",
+    appId: "1:230725901175:web:0d5f3cc4af55dc84a01f72",
+    measurementId: "G-HGZVJSDBTX"
   };
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
@@ -41,34 +41,3 @@ const db = getDatabase();
 const dbLocation = {};
 dbLocation['/deck/flashcards/'] = myFlashcards;
 update (ref(db), dbLocation);
-
-
-firebase.database().ref();  // Specifies the database root
-firebase.database().ref("child/path") // Specifies a specific location in the database tree
-
-let myFlashcards = [ {
-    "question": "Name this species of bird.",
-    "image" : "images/barn-owl-2550068__340.webp",
-    "answer": "Barn Owl"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/dove-2516641__340.webp",
-    "answer": "Dove"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/hummingbird-2139279__340.webp",
-    "answer": "Hummingbird"
-    }
-  ];
-
-// Make the database point to the location root -> deck -> flashcards
-// If the location doesn't exist is will be created
-// Create the reference location
-let dbLocation = firebase.database().ref('deck/flashcards'); 
- 
-// myFlashcards will be stored under flashcards in the database
-// Anything that was in this location will be overwritten
-// Thus, a write operation also does an update
-dbLocation.set(myFlashcards);
