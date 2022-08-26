fb40f7492aa53871384f720b3df57e6337224d4f
sunqiiii <108568986+sunqiiii@users.noreply.github.com>
2022-08-27 01:58:43 +0800

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,22 +1,3 @@
-import {getDatabase, ref, set, update, child, get,
-onValue, remove} from "https://www.gstatic.com/firebasejs/9.9.2/firebasedatabase.js";
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
-function initializeFirebase() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyDW93BmUlsdrp3MBc_rBRLPKeZViv9OAk4",
-    authDomain: "best-learning-tool-42ca7.firebaseapp.com",
-    databaseURL: "https://best-learning-tool-42ca7-default-rtdb.firebaseio.com",
-    projectId: "best-learning-tool-42ca7",
-    storageBucket: "best-learning-tool-42ca7.appspot.com",
-    messagingSenderId: "230725901175",
-    appId: "1:230725901175:web:0d5f3cc4af55dc84a01f72",
-    measurementId: "G-HGZVJSDBTX"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
-
 initializeFirebase();
 
 let myFlashcard = [
